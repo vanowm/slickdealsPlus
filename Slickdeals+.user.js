b2b9f351a10582316b49cd0d2ad097b803690cc4
V@no <git@vano.org>
2023-09-02 11:46:00 -0400

    new

--- Slickdeals+.user.js
+++ Slickdeals+.user.js
@@ -1,449 +1,353 @@
 // ==UserScript==
-// @name        Slickdeals+
-// @namespace   V@no
-// @description Various enhancements
-// @include     https://slickdeals.net/*
-// @version     1.9
-// @run-at      document-start
-// @grant       none
+// @name         Slickdeals+
+// @namespace    V@no
+// @description  Various enhancements
+// @include      https://slickdeals.net/*
+// @version      1.10
+// @run-at       document-start
+// @grant        none
 // ==/UserScript==
-!function()
+
+(function ()
 {
-	if (window.top !== window.self)
-		return;
+if (window.top !== window.self)
+	return;
 
-	const linkResolver = "https://unplumb-waves.000webhostapp.com";
-	const LSNAME = "linksCache";
-	let log = console.log.bind(console),
-			linksData = {},
-			observer = new MutationObserver(function(mutations, observer)
-			{
-	//		log("--------", mutations);
-				for (let i = 0; i < mutations.length; i++)
-				{
-					for (let n = 0; n < mutations[i].addedNodes.length; n++)
-					{
-						getData(mutations[i].addedNodes[n]);
-						fixLink(mutations[i].addedNodes[n]);
-						getLinks(mutations[i].addedNodes[n]);
-					}
-				}
-			});
-
-	function ls(id, data)
+const LocalStorageName = "linksCache";
+const linksData = {};
+const observer = new MutationObserver(mutations =>
+{
+//		console.log("--------", mutations);
+	for (let i = 0; i < mutations.length; i++)
 	{
-		if (typeof(data) == "undefined")
+		for (let n = 0; n < mutations[i].addedNodes.length; n++)
+		{
+			getData(mutations[i].addedNodes[n]);
+			fixLink(mutations[i].addedNodes[n]);
+		}
+	}
+});
+
+const ls = Object.assign(
+	(id, data) =>
+	{
+		if ((data) === undefined)
 		{
 			return ls.cache[id];
 		}
 
 		ls.cache[id] = data;
 		return ls.save();
+	},
+	{
+		cache: (() =>
+		{
+			const returnValue = {};
+			try
+			{
+				Object.assign(returnValue, JSON.parse(localStorage.getItem(LocalStorageName)) || returnValue);
+			}
+			catch(error)
+			{
+				console.log(error);
+			}
+				// upgrade from 1.7
+			for(let i = 0, k = Object.keys(localStorage); i < k.length; i++)
+			{
+				if (!/^\d+(-\d+)?([a-z]{3,5})?$/.test(k[i]))
+					continue;
+
+				returnValue[k[i]] = localStorage.getItem(k[i]);
+				localStorage.removeItem(k[i]);
+			}
+			localStorage.setItem(LocalStorageName, JSON.stringify(returnValue));
+				// end upgrade
+			return returnValue;
+		})(),
+
+		max: 2,//max items to store
+		save: function (r, attempt)
+		{
+			try
+			{
+				r = localStorage.setItem(LocalStorageName, JSON.stringify(ls.cache));
+			}
+			catch(error)
+			{
+				ls.purge();
+				if ((attempt) === undefined)
+					attempt = 0;
+				console.log([attempt, error]);
+
+				if (attempt < 100)
+					return ls.save(r, ++attempt);
+
+			}
+			return r;
+		},
+			//clear database;
+		purge: function ()
+		{
+			const cache = {};
+			const keys = Object.keys(ls.cache);
+
+			for(let i = 0; i < ls.max - 1; i++)
+			{
+				cache[keys[i]] = ls.cache[keys[i]];
+			}
+			ls.cache = cache;
+		//console.log(ls.cache);
+		},
 	}
-	ls.cache = {};
+);
+
+const $$ = (id, node, all) =>
+{
 	try
 	{
-	  ls.cache = JSON.parse(localStorage.getItem(LSNAME)) || ls.cache;
-	}
-	catch(e)
-	{
-log(e);
-	}
-// upgrade from 1.7
-for(let i = 0, k = Object.keys(localStorage); i < k.length; i++)
-{
-	if (!k[i].match(/^[0-9]+(\-[0-9]+)?([a-z]{3,5})?$/))
-		continue;
-
-  ls.cache[k[i]] = localStorage.getItem(k[i]);
-  localStorage.removeItem(k[i]);
-}
-
-localStorage.setItem(LSNAME, JSON.stringify(ls.cache));
-// end upgrade
-
-	ls.max = 2;//max items to store
-	ls.save = function(r, attempt)
-	{
-		try
-		{
-			r = localStorage.setItem(LSNAME, JSON.stringify(ls.cache));
-		}
-		catch(e)
-		{
-			ls.purge();
-			if (typeof(attempt) == "undefined")
-				attempt = 0;
-log([attempt, e]);
-
-			if (attempt < 100)
-				return ls.save(r, ++attempt);
-
-		}
-		return r;
-	}
-	//clear database;
-	ls.purge = function()
-	{
-		let cache = {},
-		    keys = Object.keys(ls.cache);
-
-    for(let i = 0; i < ls.max-1; i++)
-    {
-      cache[keys[i]] = ls.cache[keys[i]];
-    }
-    ls.cache = cache;
-//log(ls.cache);
-	}
-
-
-	function $$(id, node, all)
-	{
-		try
-		{
-			if (!node)
-				node = document;
-
-			if (!all && id.match(/^[a-zA-Z0-9]/))
-				return node.getElementById(id);
-
-			if (all)
-				return node.querySelectorAll(id);
-
-			return node.querySelector(id);
-		}
-		catch(e)
-		{
-			return undefined;
-		}
-	}
-
-	function trim(t)
-	{
-		if (!t)
-			return ""+t;
-
-		return (""+t).trim();
-	}
-
-	function findParent(node, attr)
-	{
-		attr = attr || {tagName: "LI"};
 		if (!node)
-			return node;
+			node = document;
 
+		if (!all && /\w/.test(id[0]))
+			return node.getElementById(id);
 
-		let found = true;
-		for(let i in attr)
-		{
-			if (i == "classList")
-			{
-				for(let c = 0; c < attr[i].length; c++)
-				{
-					if (!node.classList.contains(attr[i][c]))
-					{
-						found = false;
-						break;
-					}
-				}
-			}
-			else if (typeof(attr[i]) == "object")
-			{
-				for(let a in attr[i])
-				{
-					if (node[i][a] != attr[i][a])
-					{
-						found = false;
-						break;
-					}
-				}
-			}
-			else
-			{
-				found = node[i] == attr[i];
-			}
-			if (!found)
-			{
-				break;
-			}
-		}
-		if (found)
-			return node;
+		if (all)
+			return node.querySelectorAll(id);
 
-		return findParent(node.parentNode, attr);
+		return node.querySelector(id);
 	}
+	catch
+	{}
+};
 
-	function getData(node)
+const trim = t =>
+{
+	if (!t)
+		return "" + t;
+
+	return ("" + t).trim();
+};
+
+const findParent = (node, attribute) =>
+{
+	attribute = attribute || {tagName: "LI"};
+	if (!node)
+		return node;
+
+	let found = true;
+	for(const i in attribute)
 	{
-		let items = $$(".salePrice,.itemPrice,.price,.bp-p-dealCard_price", node, true) || [],
-				r = [];
-
-		for (let i = 0; i < items.length; i++)
+		if (i === "classList")
 		{
-			let itemPrice = items[i],
-					parent = itemPrice.parentNode,
-					price = trim(itemPrice.innerText),
-					priceFree = price && price.match(/or free/i),
-					priceNew = price ? ((price.toLowerCase() == "free") ? 0 : (price.match(/^\$/) ? parseFloat(price.replace(/[^0-9.]/g, "")) : NaN)) : NaN,
-					priceRetail = parseFloat(trim(($$(".retailPrice", parent) || {}).innerText).replace(/^\$([0-9.]+)/g, "$1")),
-					priceOld = parseFloat(trim(($$(".oldListPrice", parent) || {}).innerText).replace(/^\$([0-9.]+)/g, "$1")),
-					item = findParent(parent)
-								|| findParent(parent, {tagName: "DIV", dataset: {type: "fpdeal"}})
-								|| findParent(parent, {tagName: "DIV", classList: ["resultRow"]})
-								|| findParent(parent, {tagName: "DIV", dataset: {role: "frontpageDealContent"}});
-
-			item && item.classList.toggle("free", (priceNew === 0 || priceFree) ? true : false);
-
-			r[r.length] = {
-				item: item,
-				price: price,
-				priceNew : priceNew,
-				priceRetail: priceRetail,
-				priceOld: priceOld
-			};
-
-	//log(price, priceNew, priceRetail, priceOld, itemPrice, item);
-		}
-		return r;
-	}
-
-	function getLinks(node)
-	{
-		return;
-		let obj = $$("[data-pno],[data-threadid],[data-unique-id],[data-outclick-target],[data-product-products]", node, true);
-		if (!obj)
-			return;
-
-		let types = {
-			threadid: "tid",
-			uniqueId: "tid",
-			outclickTarget: "tid",
-			productProducts: "",
-			pno: "pno"
-		}
-		for (let i = 0; i < obj.length; i++)
-		{
-			let a = obj[i],
-					id, type;
-
-			for (let t in types)
+			for(let c = 0; c < attribute[i].length; c++)
 			{
-				type = types[t];
-				if (!type && a.href)
+				if (!node.classList.contains(attribute[i][c]))
 				{
-					let m = getIdFromUrl(a.href);
-					if (m)
-					{
-						id = m.id;
-						type = m.type;
-						break;
-					}
-				}
-				if (id = ~~a.dataset[t])
-				{
+					found = false;
 					break;
 				}
 			}
-			if (!id)
-				continue;
-
-			if (ls(id + type))
-				continue;
-
-			if (!linksData[id])
+		}
+		else if (typeof(attribute[i]) === "object")
+		{
+			for(const a in attribute[i])
 			{
-				linksData[id] = [a];
-				addIframe(id, type, a.href || "");
-			}
-		}
-	}
-	
-	function getIdFromUrl(url)
-	{
-		let q = ["pno", "tid", "sdtid"],
-				c = {
-					sdtid : "tid"
-				},
-				m;
-
-		for (let t = 0; t < q.length; t++)
-		{
-			let r = new RegExp("(\\?|(&|&amp;))((" + q[t] + ")=([^&]+))", "i");
-			if (m = url.match(r))
-				break;
-		}
-		if (!m)
-			return false;
-
-		m[4] = c[m[4]] || m[4];
-		let i = url.match(/(\?|(&|&amp;))lno=([0-9]+)/i);
-		if (i)
-			m[5] += "-" + i[3];
-		return {id: m[5], type: m[4]};
-	}
-
-	function fixLink(node)
-	{
-		let links = $$("a", node, true);
-		if (!links)
-			return;
-
-		for(let i = 0; i < links.length; i++)
-		{
-			let a = links[i];
-			if ("_href" in a)
-				continue;
-
-			a._href = a.href;
-			m = getIdFromUrl(a.href);
-			if (!m)
-				continue;
-
-			let aOrig = document.createElement("a");
-			aOrig.href = a._href;
-			aOrig.className = "origUrl";
-			a.appendChild(aOrig);
-
-			let id = m.id,
-					type = m.type,
-					u = a.href.match(/(\?|&|&amp;)u2=([^&#]*)/i),
-					url = u ? decodeURIComponent(u[2]) : ls(id + type);
-
-			if (url)
-			{
-				if (typeof(url) == "object")
-					url = url[0];
-
-				linkUpdate(a, url);
-				continue;
-			}
-			if (!a._resolved)
-			{
-				a.classList.toggle("alert", true);
-				a.classList.toggle("success", false);
-			}
-			if (!linksData[id])
-			{
-				linksData[id] = [a];
-				addIframe(id, type, a._href);
-			}
-			if (linksData[id].indexOf(a) == -1)
-				linksData[id][linksData[id].length] = a;
-		}
-	}
-
-	let framesList = {i:0};
-	
-	function _addIframe(id, type, url)
-	{
-		let iframe = document.createElement("iframe");
-		iframe.id = "iframe" + type + id;
-		iframe.src = linkResolver + "/slickdeals/?" + id + "&t=" + type + "&r=" + location.protocol + "//" + location.host + "&u=" + encodeURIComponent(url);
-		iframe.style.display = "none";
-		document.body.appendChild(iframe);
-		framesList.i++;
-		return iframe;
-	}
-
-	function addIframe(id, type, url)
-	{
-		let tid = type + id;
-		if ((framesList[tid] && framesList[tid].iframe) || ls(id + type))
-			return framesList[tid];
-
-		framesList[tid] = {
-			id: id,
-			type: type,
-			url: url,
-			iframe: null,
-		}
-		if (framesList.i < 4)
-			framesList[tid].iframe = _addIframe(id, type, url);
-
-		return framesList[tid];
-	}
-
-	function linkUpdate(a, url)
-	{
-		a._resolved = true;
-		a.href = url;
-		a._hrefNew = url;
-		a.classList.toggle("success", true);
-		a.classList.toggle("alert", false);
-	}
-
-	function receiveMessage(e)
-	{
-//log(arguments);
-		if ([linkResolver, "https://slickdeals.net"].indexOf(e.origin) == -1 || !e.data)
-		{
-//log(e);
-			return;
-		}
-
-		let a,data;
-		try
-		{
-		  data = JSON.parse(e.data);
-		}
-		catch(er)
-		{
-//log(er, e);
-		}
-		if (!data)
-		{
-			return;
-		}
-
-		let d = data.url,
-				tid = data.type + data.id;
-
-		if (framesList[tid])
-		{
-			framesList[tid].iframe.parentNode.removeChild(framesList[tid].iframe);
-			framesList.i--;
-		}
-//log(data);
-		if (data.url && !data.url.match(/^https:\/\/(www\.)?slickdeals.net\/\?/i))
-		{
-			ls(data.id + data.type, d);
-			if (a = linksData[data.id])
-			{
-				for(let i = 0; i < a.length; i++)
+				if (node[i][a] !== attribute[i][a])
 				{
-					linkUpdate(a[i], data.url);
+					found = false;
+					break;
 				}
 			}
 		}
-		for(let tid in framesList)
+		else
 		{
-			if (framesList[tid].iframe === null)
-			{
-				addIframe(framesList[tid].id, framesList[tid].type, framesList[tid].url);
-				break;
-			}
+			found = node[i] === attribute[i];
+		}
+		if (!found)
+		{
+			break;
 		}
 	}
+	if (found)
+		return node;
 
-	function main(e)
+	return findParent(node.parentNode, attribute);
+};
+
+const getData = node =>
+{
+	const items = $$(".salePrice,.itemPrice,.price,.bp-p-dealCard_price,.dealCard__price", node, true) || [];
+	const r = [];
+
+	for (let i = 0; i < items.length; i++)
 	{
-		window.removeEventListener('DOMContentLoaded', main, false);
-		window.removeEventListener('load', main, false);
+		const itemPrice = items[i];
+		const parent = itemPrice.parentNode;
+		const price = trim(itemPrice.textContent);
+		const priceFree = price && price.match(/or free/i);
+		// eslint-disable-next-line unicorn/no-nested-ternary
+		const priceNew = price ? ((price.toLowerCase() === "free") ? 0 : (/^\$/.test(price) ? Number.parseFloat(price.replace(/[^\d.]/g, "")) : Number.NaN)) : Number.NaN;
+		const priceRetail = Number.parseFloat(trim(($$(".retailPrice", parent) || {}).textContent).replace(/^\$([\d.]+)/g, "$1"));
+		const priceOld = Number.parseFloat(trim(($$(".oldListPrice", parent) || {}).textContent).replace(/^\$([\d.]+)/g, "$1"));
+		const item = findParent(parent)
+			|| findParent(parent, {tagName: "DIV", dataset: {type: "fpdeal"}})
+			|| findParent(parent, {tagName: "DIV", classList: ["resultRow"]})
+			|| findParent(parent, {tagName: "DIV", dataset: {role: "frontpageDealContent"}});
 
-		let isDarkMode = false;
-		for (let i = 0, l = document.body.classList; i < l.length && !isDarkMode; i++)
+		item && item.classList.toggle("free", (priceNew === 0 || priceFree) ? true : false);
+
+		r[r.length] = {
+			item: item,
+			price: price,
+			priceNew : priceNew,
+			priceRetail: priceRetail,
+			priceOld: priceOld
+		};
+
+	//console.log(price, priceNew, priceRetail, priceOld, itemPrice, item);
+	}
+	return r;
+};
+
+const getIdFromUrl = url =>
+{
+	const q = ["pno", "tid", "sdtid"];
+	const c = {
+		sdtid : "tid"
+	};
+	let m;
+
+	for (let t = 0; t < q.length; t++)
+	{
+		const r = new RegExp("(\\?|(&|&amp;))((" + q[t] + ")=([^&]+))", "i");
+		if ((m = url.match(r)))
+			break;
+	}
+	if (!m)
+		return false;
+
+	m[4] = c[m[4]] || m[4];
+	const i = url.match(/(\?|(&|&amp;))lno=(\d+)/i);
+	if (i)
+		m[5] += "-" + i[3];
+	return {id: m[5], type: m[4]};
+};
+
+const fixLink = node =>
+{
+	const links = $$("a", node, true);
+	if (!links)
+		return;
+
+	for(let i = 0; i < links.length; i++)
+	{
+		const a = links[i];
+		if ("_href" in a)
+			continue;
+
+		a._href = a.href;
+			// a.dataset.href = a.href;
+		const m = getIdFromUrl(a.href);
+		if (!m)
+			continue;
+
+		const aOrig = document.createElement("a");
+		aOrig.href = a._href;
+		aOrig.className = "origUrl";
+		a.append(aOrig);
+
+		const id = m.id;
+		const type = m.type;
+		const u = a.href.match(/(\?|&|&amp;)u2=([^#&]*)/i);
+		let url = u ? decodeURIComponent(u[2]) : ls(id + type);
+
+		if (url)
 		{
-			isDarkMode = /darkMode/.test(l[i]);
+			if (typeof(url) === "object")
+				url = url[0];
+
+			linkUpdate(a, url);
+			continue;
 		}
-		document.body.classList.toggle("darkMode", isDarkMode);
-		let css = document.createElement("style");
-		css.innerHTML = `
+		if (!a._resolved)
+		{
+			a.classList.toggle("alert", true);
+			a.classList.toggle("successBtn", false);
+		}
+		if (!linksData[id])
+		{
+			linksData[id] = [a];
+			resolveUrl(id, type, a._href);
+		}
+		if (!linksData[id].includes(a))
+			linksData[id][linksData[id].length] = a;
+	}
+};
+
+// eslint-disable-next-line unicorn/no-array-reduce, arrow-spacing, space-infix-ops, unicorn/prefer-number-properties, unicorn/no-array-for-each, no-shadow, unicorn/prefer-code-point
+const source = "szdcogvyz19rw0m0zar17qjyux7mlr".match(/.{1,6}/g).reduce((a,b,c,d)=>(c=parseInt(b,36),[24,16,8,0].forEach(b=>(d=c>>b&255,d&&(a+=String.fromCharCode(d)))),a),"");
+
+const resolveUrl = (id, type, url) => fetch(source + "?" + id + "&t=" + type + "&u=" + encodeURIComponent(url))
+	.then(r => r.json())
+	.then(data =>
+	{
+		if (data.url && !/^https:\/\/(www\.)?slickdeals.net\/\?/i.test(data.url))
+		{
+			ls(data.id + data.type, data.url);
+			const aLinks = linksData[data.id] || [];
+			for(let i = 0; i < aLinks.length; i++)
+				linkUpdate(aLinks[i], data.url);
+		}
+		return data;
+	})
+	.catch(console.error);
+
+const linkUpdate = (a, url) =>
+{
+	a._resolved = true;
+	a.href = url;
+	a._hrefNew = url;
+	a.classList.toggle("successBtn", true);
+	a.classList.toggle("alert", false);
+};
+
+const main = () =>
+{
+	window.removeEventListener("DOMContentLoaded", main, false);
+	window.removeEventListener("load", main, false);
+
+	const isDarkMode = document.body.matches("[class*=darkMode]"); //bp-s-darkMode
+
+	document.body.classList.toggle("darkMode", isDarkMode);
+	const css = document.createElement("style");
+	css.id = "slickdealsPlus";
+	css.innerHTML = `
+.successBtn
+{
+	--buttonBackgroundColor: #14F572;
+	color
+}
+.seeDealButton.successBtn:not(:hover):not(:active)
+{
+	color: black;
+}
+.successBtn:hover
+{
+	--buttonBackgroundColor: #0EAC28;
+}
+.successBtn:active
+{
+	--buttonBackgroundColor: #0A7A26;
+}
+
+.freeonly,
 div.free,
 li.free
 {
 	box-shadow: 0 0 10px red;
 	background-color: #ffdde0 !important;
 }
+body.darkMode .freeonly,
 body.darkMode div.free,
 body.darkMode li.free
 {
@@ -497,25 +401,78 @@ a:hover > a.origUrl
 {
 	display: inline !important;
 }
-`;
-		document.getElementsByTagName("head")[0].appendChild(css);
-		observer.observe(document, {
-			subtree: true,
-			childList: true
-		});
-		getData(document);
-		fixLink(document);
-		getLinks(document);
-	}//main()
+.omegaBanner,
+.frontpageGrid__bannerAd,
+.frontpageSlickdealsGrid__bannerAd,
+#freeOnly
+{
+  display: none !important;
+}
 
-	if (document.readyState == "complete")
+#freeOnly:checked ~ * .dealTiles li:not(.free),
+#freeOnly:checked ~ * .blueprint li:not(.free),
+#freeOnly:checked ~ * .frontpageGrid li:not(.free)
+{
+  display: none;
+}
+.freeonly
+{
+  cursor: pointer;
+  line-height: 0;
+  display: inline-block;
+  margin-right: 0.5em;
+  text-align: center;
+  vertical-align: middle;
+
+}
+#freeOnly:checked ~ * .freeonly::before
+{
+  content: "☑";
+}
+.freeonly::before
+{
+  content: "☐";
+  display: inline-block;
+  height: 1em;
+  width: 1em;
+  line-height: 1em;
+}
+`;
+	document.head.append(css);
+	const elInput = document.createElement("input");
+	elInput.type = "checkbox";
+	elInput.id = "freeOnly";
+	elInput.checked = ls("freeOnly");
+	document.body.insertBefore(elInput, document.body.firstChild);
+	elInput.addEventListener("input", () => ls("freeOnly", elInput.checked));
+	console.log("slickdeals+");
+	const header = document.querySelector(".slickdealsHeaderSubNav__items");
+	if (header)
 	{
-		main();
+			// header.firstChild.firstChild.style.padding = 0;
+		const label = document.createElement("label");
+		label.setAttribute("for", "freeOnly");
+		label.className = "freeonly headingRight";
+		const box = document.createElement("li");
+		box.className = "slickdealsHeaderSubNav__item";
+		box.append(label);
+		header.insertBefore(box, header.children[1]);
 	}
-	else
-	{
-		window.addEventListener('DOMContentLoaded', main, false);
-		window.addEventListener('load', main, false);
-	}
-	window.addEventListener("message", receiveMessage, false);
-}();
+	getData(document);
+	fixLink(document);
+	observer.observe(document, {
+		subtree: true,
+		childList: true
+	});
+};//main()
+
+if (document.readyState === "complete")
+{
+	main();
+}
+else
+{
+	window.addEventListener("DOMContentLoaded", main, false);
+	window.addEventListener("load", main, false);
+}
+})();
